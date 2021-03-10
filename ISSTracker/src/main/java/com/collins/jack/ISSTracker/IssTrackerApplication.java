0244144de7863f58ed578930783a0aaaf076c48c
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2021-03-10 16:40:45 +0000

    Error handling and more
    Updated error handling, fixed the iss_position junk, added in LocationService

--- ISSTracker/src/main/java/com/collins/jack/ISSTracker/IssTrackerApplication.java
+++ ISSTracker/src/main/java/com/collins/jack/ISSTracker/IssTrackerApplication.java
@@ -2,8 +2,10 @@ package com.collins.jack.ISSTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class IssTrackerApplication {
 
 	public static void main(String[] args) {
--- ISSTracker/src/main/java/com/collins/jack/ISSTracker/controllers/ISSController.java
+++ ISSTracker/src/main/java/com/collins/jack/ISSTracker/controllers/ISSController.java
@@ -1,26 +1,21 @@
 package com.collins.jack.ISSTracker.controllers;
 
 import com.collins.jack.ISSTracker.models.IssLocator;
+import com.collins.jack.ISSTracker.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
 public class ISSController {
 
-    private static final String FAILED = "failed";
+    @Autowired
+    private LocationService locationService;
 
     @GetMapping("/issLocation")
     public IssLocator getISSLocation() {
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            return restTemplate.getForObject("http://api.open-notify.org/iss-now.json", IssLocator.class);
-        } catch (Exception e) {
-            IssLocator issLocator = new IssLocator();
-            issLocator.setMessage(FAILED);
-            return issLocator;
-        }
+        return this.locationService.getIssLocation();
     }
 }
