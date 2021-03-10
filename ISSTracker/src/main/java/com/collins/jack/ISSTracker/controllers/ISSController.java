b31a4f883350c049c6485fd0af7e153b0c5097e7
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2021-03-10 08:34:31 +0000

    Add files via upload

---
+++ ISSTracker/src/main/java/com/collins/jack/ISSTracker/controllers/ISSController.java
@@ -0,0 +1,23 @@
+package com.collins.jack.ISSTracker.controllers;
+
+import com.collins.jack.ISSTracker.models.IssLocator;
+import com.collins.jack.ISSTracker.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ISSController {
+
+    private static final String FAILED = "failed";
+
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping("/issLocation")
+    public IssLocator getISSLocation() {
+        return this.locationService.getIssLocation();
+    }
+}
---
+++ ISSTracker/src/test/java/com/collins/jack/ISSTracker/IssTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collins.jack.ISSTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
