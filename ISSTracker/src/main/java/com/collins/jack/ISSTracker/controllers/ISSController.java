5e9d471d4f682d88c8a1b3fcf8c1aee672add04c
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2021-03-11 15:20:02 +0000

    hmtl and js added in
    Basically finishing the basic project

--- ISSTracker/src/main/java/com/collins/jack/ISSTracker/controllers/ISSController.java
+++ ISSTracker/src/main/java/com/collins/jack/ISSTracker/controllers/ISSController.java
@@ -3,6 +3,7 @@ package com.collins.jack.ISSTracker.controllers;
 import com.collins.jack.ISSTracker.models.IssLocator;
 import com.collins.jack.ISSTracker.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,8 @@ public class ISSController {
     @Autowired
     private LocationService locationService;
 
-    @Autowired
-    private LocationService locationService;
-
     @GetMapping("/issLocation")
+    @CrossOrigin
     public IssLocator getISSLocation() {
         return this.locationService.getIssLocation();
     }
